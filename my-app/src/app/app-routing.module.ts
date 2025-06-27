f45a7339d5062dd60ed8e528c0f2200e164e64e4
Reshma9911 <areshmashareen@gmail.com>
2025-06-27 17:34:51 +0530

    updated api url in service folder

--- my-app/src/app/app-routing.module.ts
+++ my-app/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AccountComponent } from './account/account.component';
+import { BankComponent } from './bank/bank.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+   { path: 'account', component: AccountComponent },
+  { path: 'bank', component: BankComponent },
+  { path: '', redirectTo: '/account', pathMatch: 'full' }, // default route
+  { path: '**', redirectTo: '/account' } // wildcard route
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ my-app/src/app/common/comman-service.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Account } from './common.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CommanServiceService {
+  private apiUrl = 'https://jsonplaceholder.typicode.com/users'; // dummy API
+
+  constructor( private http: HttpClient) { }
+
+
+
+  getAccounts(): Observable<Account[]> {
+    return this.http.get<Account[]>(this.apiUrl);
+  }
+  
+  public method()
+  {
+    console.log("api",this.apiUrl);
+  }
+}
